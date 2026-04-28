0ae251b195d112f5e9adf71187f5c7dec678c097
Henrique Flores <henriquemfl@hotmail.com>
2026-04-28 17:49:37 +0200

    Renaming binary

--- PageParser/build.bat
+++ PageParser/build.bat
@@ -8,6 +8,6 @@ CALL "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\v
 
 PUSHD "build"
     REM cl /EHsc /Zi /Wall /wd4091 /Fe:PageParser.exe ..\PageParser\main.c ..\PageParser\tex.c ..\PageParser\html.c
-    cl /EHsc /Zi /Fe:PageParser.exe ..\PageParser\main.c ..\PageParser\page_parser.c ..\PageParser\tex.c ..\PageParser\html.c
+    cl /EHsc /Zi /Fe:page_parser.exe ..\PageParser\main.c ..\PageParser\page_parser.c ..\PageParser\tex.c ..\PageParser\html.c
     cl /EHsc /Zi /Fe:tests.exe ..\PageParser\tests.c ..\PageParser\page_parser.c ..\PageParser\tex.c ..\PageParser\html.c
 POPD
